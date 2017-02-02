0a47945fe80c765b9fb3619ae9fe66abc8c08136
raguilar66 <raguilar@monlau.es>
2017-02-02 18:24:25 +0100

    Primer commit

---
+++ EjemploMaven/src/main/java/com/ramon/maven/EjemploMaven/App.java
@@ -0,0 +1,34 @@
+package com.ramon.maven.EjemploMaven;
+import java.sql.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try {
+    		Class.forName("com.mysql.jdbc.Driver");
+    		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/DAO","ramon","monlau");
+    		System.out.println( "Esta conectado a mysql" );
+    		System.out.println();  
+    		Statement orden = con.createStatement();
+    		
+    		ResultSet resul = orden.executeQuery("SELECT * from departamentos");
+    		System.out.println( "Lista de Departamentos" );
+    		System.out.println( "----------------------" );
+    		System.out.println();
+    		while (resul.next()) {
+    			System.out.println(resul.getInt("dept_no")+" "+resul.getString(2)+" "+resul.getString("loc"));
+    		}
+    		resul.close();
+    		orden.close();
+    		con.close();
+    	}
+    	catch (ClassNotFoundException cn) {cn.printStackTrace();}
+    	catch (SQLException e) {
+    		System.out.println("Ha ocurrido un error:");
+    		System.out.println("Mensaje: " + e.getMessage());
+    		System.out.println("SQL estado: " + e.getSQLState());
+    		System.out.println("Codigo de error: " + e.getErrorCode());
+    	}       
+    }
+}
